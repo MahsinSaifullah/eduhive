b8e32b573e53f98722814ed0cab4fa5da02757df
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-12-03 12:31:16 +0000

    feat: add Sidebar UI

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,19 @@
+import Logo from '@/components/Logo';
+import Menu from '@/components/Menu';
+import Navbar from '@/components/Navbar';
 import { PropsWithChildren } from 'react';
 
 const DashboardLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="h-screen flex">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">l</div>
-      <div className="w-[86%]  md:w-[92%] lg:w-[84%] xl:w-[86%]">r</div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Logo />
+        <Menu />
+      </div>
+      <div className="w-[86%]  md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 };
---
+++ src/components/Logo.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Logo = () => {
+  return (
+    <Link
+      href="/"
+      className="flex items-center justify-center gap-2 lg:justify-start"
+    >
+      <Image src="/logo.png" alt="eduhive logo" width={32} height={32} />
+      <span className="hidden lg:block">EduHive</span>
+    </Link>
+  );
+};
+
+export default Logo;
