23fe1ecc109cc103d8c91875554336d30fec3ea6
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-12-05 17:53:31 +0000

    feat: add RLS check to menu items

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,5 @@
 import { menuItems } from '@/constants/menuItems';
+import { role } from '@/lib/data';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -10,16 +11,25 @@ const Menu = () => {
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {i.title}
           </span>
-          {i.items.map((item) => (
-            <Link
-              href={item.href}
-              key={item.label}
-              className="flex items-center justify-center lg:justify-start gap-4 py-2 text-gray-500"
-            >
-              <Image src={item.icon} alt={item.label} width={20} height={20} />
-              <span className="hidden lg:block">{item.label}</span>
-            </Link>
-          ))}
+          {i.items.map((item) => {
+            return (
+              item.visible.includes(role) && (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 py-2 text-gray-500"
+                >
+                  <Image
+                    src={item.icon}
+                    alt={item.label}
+                    width={20}
+                    height={20}
+                  />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              )
+            );
+          })}
         </div>
       ))}
     </aside>
