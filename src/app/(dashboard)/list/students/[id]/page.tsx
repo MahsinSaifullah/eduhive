8c7a7465abd47c4e43ed8f4bf317a3a5c8fe3d52
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-12-21 09:21:21 +0000

    feat: add single student page

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-const SingleTeacherPage = () => {
+const SingleStudentPage = () => {
   return (
     <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
       <div className="w-full xl:w-2/3">
@@ -13,7 +13,7 @@ const SingleTeacherPage = () => {
           <div className="bg-sky py-6 px-4 rounded-md flex-1 flex gap-4">
             <div className="w-1/3">
               <Image
-                src="https://images.pexels.com/photos/5414817/pexels-photo-5414817.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200"
                 alt=""
                 width={144}
                 height={144}
@@ -21,7 +21,7 @@ const SingleTeacherPage = () => {
               />
             </div>
             <div className="w-2/3 flex flex-col justify-between gap-4">
-              <h1 className="text-xl font-semibold">Leonard Snyder</h1>
+              <h1 className="text-xl font-semibold">Allie Summers</h1>
               <p className="text-sm text-gray-500">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               </p>
@@ -36,7 +36,7 @@ const SingleTeacherPage = () => {
                 </div>
                 <div className="flex items-center gap-2 w-full md:w-1/3 lg:w-full 2xl:w-1/3">
                   <Image src="/mail.png" alt="" width={14} height={14} />
-                  <span>leonard.snyder@eduhive.com</span>
+                  <span>allie.summers@eduhive.com</span>
                 </div>
                 <div className="flex items-center gap-2 w-full md:w-1/3 lg:w-full 2xl:w-1/3">
                   <Image src="/phone.png" alt="" width={14} height={14} />
@@ -55,7 +55,7 @@ const SingleTeacherPage = () => {
                 className="w-6 h-6"
               />
               <div>
-                <h1 className="text-xl font-semibold">90%</h1>
+                <h1 className="text-xl font-semibold">94%</h1>
                 <span className="text-sm text-gray-400">Attendance</span>
               </div>
             </div>
@@ -68,8 +68,8 @@ const SingleTeacherPage = () => {
                 className="w-6 h-6"
               />
               <div>
-                <h1 className="text-xl font-semibold">2</h1>
-                <span className="text-sm text-gray-400">Branches</span>
+                <h1 className="text-xl font-semibold">6th</h1>
+                <span className="text-sm text-gray-400">Grade</span>
               </div>
             </div>
             <div className="bg-white p-4 rounded-md w-full flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
@@ -81,7 +81,7 @@ const SingleTeacherPage = () => {
                 className="w-6 h-6"
               />
               <div>
-                <h1 className="text-xl font-semibold">6</h1>
+                <h1 className="text-xl font-semibold">18</h1>
                 <span className="text-sm text-gray-400">Lessons</span>
               </div>
             </div>
@@ -94,14 +94,14 @@ const SingleTeacherPage = () => {
                 className="w-6 h-6"
               />
               <div>
-                <h1 className="text-xl font-semibold">6</h1>
-                <span className="text-sm text-gray-400">Classes</span>
+                <h1 className="text-xl font-semibold">6A</h1>
+                <span className="text-sm text-gray-400">Class Name</span>
               </div>
             </div>
           </div>
         </div>
-        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-          <h1>Teacher&apos;s Schedule</h1>
+        <div className="mt-4 bg-white rounded-md p-4 h-[800px] font-semibold">
+          <h1>Student&apos;s Schedule</h1>
           <BigCalendar />
         </div>
       </div>
@@ -109,20 +109,20 @@ const SingleTeacherPage = () => {
         <div className="bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Shortcuts</h1>
           <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-            <Link className="p-3 rounded-md bg-lightSky" href="/">
-              Teacher&apos;s Classes
+            <Link className="p-3 rounded-md bg-lightLemon" href="/">
+              Student&apos;s Lessons
             </Link>
             <Link className="p-3 rounded-md bg-lightLilac" href="/">
-              Teacher&apos;s Students
-            </Link>
-            <Link className="p-3 rounded-md bg-lightLemon" href="/">
-              Teacher&apos;s Lessons
+              Student&apos;s Teachers
             </Link>
             <Link className="p-3 rounded-md bg-pink-50" href="/">
-              Teacher&apos;s Exams
+              Student&apos;s Exams
             </Link>
             <Link className="p-3 rounded-md bg-green-50" href="/">
-              Teacher&apos;s Assignments
+              Student&apos;s Assignments
+            </Link>
+            <Link className="p-3 rounded-md bg-orange-50" href="/">
+              Student&apos;s Results
             </Link>
           </div>
         </div>
@@ -133,4 +133,4 @@ const SingleTeacherPage = () => {
   );
 };
 
-export default SingleTeacherPage;
+export default SingleStudentPage;
