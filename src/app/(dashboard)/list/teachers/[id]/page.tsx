5f9fb9e7ee827bceb974a2bcfb331d48224a9e0a
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-12-22 10:24:41 +0000

    fix: user avatar card responsiveness

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -26,19 +26,19 @@ const SingleTeacherPage = () => {
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               </p>
               <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
-                <div className="flex items-center gap-2 w-full md:w-1/3 lg:w-full 2xl:w-1/3">
+                <div className="flex items-center gap-2 w-full">
                   <Image src="/blood.png" alt="" width={14} height={14} />
                   <span>A+</span>
                 </div>
-                <div className="flex items-center gap-2 w-full md:w-1/3 lg:w-full 2xl:w-1/3">
+                <div className="flex items-center gap-2 w-full">
                   <Image src="/date.png" alt="" width={14} height={14} />
                   <span>January 2025</span>
                 </div>
-                <div className="flex items-center gap-2 w-full md:w-1/3 lg:w-full 2xl:w-1/3">
+                <div className="flex items-center gap-2 w-full">
                   <Image src="/mail.png" alt="" width={14} height={14} />
                   <span>leonard.snyder@eduhive.com</span>
                 </div>
-                <div className="flex items-center gap-2 w-full md:w-1/3 lg:w-full 2xl:w-1/3">
+                <div className="flex items-center gap-2 w-full">
                   <Image src="/phone.png" alt="" width={14} height={14} />
                   <span>+447424856554</span>
                 </div>
